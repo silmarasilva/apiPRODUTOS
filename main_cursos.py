32bba17725f4348dd14f917c5aa5c6ccab3860ed
silmarasilva <silmara.eliza@gmail.com>
2021-06-01 14:18:31 -0300

    alter port

--- main_cursos.py
+++ main_cursos.py
@@ -182,4 +182,4 @@ def not_found(error=None):
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True, host = "0.0.0.0", port = 5002)
+    app.run(debug=True, host = "0.0.0.0", port = 80)
